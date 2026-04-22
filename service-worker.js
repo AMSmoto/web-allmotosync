82a875ebdb0c7915b650389b45aac357f125420a
AMSmoto <allmotosync.ger@gmail.com>
2026-04-22 11:48:54 -0500

    1

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,13 @@
-const CACHE_NAME = 'ams-v5';
-const urlsToCache = [
-  './',
-  './index.html',
-  './AMSchr2.png'
-];
+const CACHE_NAME = 'ams-v6';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
